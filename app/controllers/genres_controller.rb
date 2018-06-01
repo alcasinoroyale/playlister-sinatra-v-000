83309b79967bf9d352e690819822d2738c009605
Alex Zdatny <alexzdatny@gmail.com>
2018-06-01 02:34:17 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -5,7 +5,7 @@ class GenresController < ApplicationController
     erb: '/genres/index'
   end
 
-  get '/artists/:slug' do
+  get '/genres/:slug' do
     @artists = Artist.find_by_slug(params[:slug])
     erb :'artists/show'
   end
