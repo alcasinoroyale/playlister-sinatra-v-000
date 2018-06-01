62493181b06eec3b1768fc9e720570828dbf78fa
Alex Zdatny <alexzdatny@gmail.com>
2018-06-01 02:41:00 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,7 +8,7 @@ class SongsController < ApplicationController
   get '/songs/new' do
     erb :'/songs/new'
   end
-  
+
   get '/songs/:slug' do
    @song = Song.find_by_slug(params[:slug])
    erb :'songs/show'
